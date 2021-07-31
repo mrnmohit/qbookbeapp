345e5fca1205567c46656a5ca832e7dd8a412b1e
parixit.rao <mohit.niwadunge@gmail.com>
2021-07-31 12:20:50 +0530

    added sail prod comand in script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-sails lift --prod > app.out.log 2> app.err.log < /dev/null &  
+npm run prod > app.out.log 2> app.err.log < /dev/null &  
